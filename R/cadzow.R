69eb1c0ac7d10793e1ed035e09788a644a6fc29e
Nikita Zvonarev <mb_93@mail.ru>
2013-09-28 16:55:08 +0400

    Initial MSSA Cadzow without tests
    Initial Cadzow tests
    
    Reworked mcadzow and 1d cadzow with .hweights

--- R/cadzow.R
+++ R/cadzow.R
@@ -17,6 +17,33 @@
 #   Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
 #   MA 02139, USA.
 
+mcadzow <- function(x, rank,
+                    eps = 1e-6, numiter = 0,
+                    ..., cache = TRUE) {
+  # Obtain the initial reconstruction of rank r
+  r <- reconstruct(x, groups = list(1:rank), ..., cache = cache)
+  stopifnot(length(r) == 1)
+  F <- r[[1]]
+  # Do the actual iterations until the convergence (or stoppping due to number
+  # of iterations)
+  it <- 0
+  repeat {
+    s <- clone(x, copy.cache = FALSE, copy.storage = FALSE)
+    FL <- .to.series.list(F, na.rm = TRUE)
+    .set(s, "F", FL)
+    r <- reconstruct(s, groups = list(1:rank), ..., cache = cache)
+    stopifnot(length(r) == 1)
+    rF <- r[[1]]
+
+    it <- it + 1
+    if ((numiter > 0 && it >= numiter) || max((F-rF)^2) < eps)
+      break
+    F <- rF
+  }
+
+  F
+}
+
 cadzow.ssa <- function(x, rank,
                        eps = 1e-6, numiter = 0,
                        ..., cache = TRUE) {
@@ -56,10 +83,29 @@ cadzow.1d.ssa <- function(x, rank,
 
   # Correct the stuff if requested
   if (correct) {
-    h1 <- hankel(F, x$window)
-    h2 <- hankel(.get(x, "F"), x$window)
+    xF <- .get(x, "F")
+    F <- sum(F * xF * .hweights(x)) / sum(xF * xF * .hweights(x)) * F
+  }
 
-    F <- sum(h1 * h2) / sum(h2 * h2) * F
+  F
+}
+
+cadzow.mssa <- function(x, rank,
+                        correct = TRUE,
+                        eps = 1e-6, numiter = 0,
+                        ..., cache = TRUE) {
+  # Get the result w/o any correction
+  fcall <- match.call(expand.dots = FALSE)
+  fcall[[1]] <- mcadzow
+  fcall$correct <- NULL
+  F <- eval(fcall, parent.frame())
+
+  # Correct the stuff if requested
+  if (correct) {
+    h1 <- unlist(.to.series.list(F, na.rm = TRUE))
+    h2 <- unlist(.to.series.list(.get(x, "F"), na.rm = TRUE))
+    
+    F <- sum(h1 * h2 * .hweights(x)) / sum(h2 * h2 * .hweights(x)) * F
   }
 
   F
